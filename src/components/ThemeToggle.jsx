0209845426a0f2bd9b56704eb7d4ca4bba1c3ac5
DavidAkerlind <david.akerlind@icloud.com>
2025-03-27 21:10:25 +0100

    Fix button text casing in ThemeToggle and add hover effect for danger buttons

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ThemeToggle = ({ isDarkMode, toggleMode }) => {
 	return (
 		<button className="mode-toggle, button" onClick={toggleMode}>
-			Toggle {isDarkMode ? 'Light' : 'Dark'} Mode
+			Toggle {isDarkMode ? 'light' : 'dark'} mode
 		</button>
 	);
 };
